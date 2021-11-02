997c89a5e8da45e747229d8976980090f372df18
Dissy Ulina <dissyulina@gmail.com>
2021-11-02 11:28:44 +0000

    Create a carousel for popular recipes in Home page

--- static/js/script.js
+++ static/js/script.js
@@ -107,3 +107,21 @@ $("#image-url").change(function(){
     loadImage();
 });
 
+/* For Carousel in Home page */
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+$(".carousel-item").first().addClass("active");

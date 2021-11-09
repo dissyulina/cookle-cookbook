c8da70579ae70286b19a5d634e246238c0e88ecc
Dissy Ulina <dissyulina@gmail.com>
2021-11-09 08:39:01 +0000

    Change Navbar to fixed, hide on scroll down and show on scroll up

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,29 @@
+/* All pages: Navbar hide on scroll down, show on scroll up
+Code was sourced from https://bootstrap-menu.com/detail-autohide.html */
+document.addEventListener("DOMContentLoaded", function(){
+    el_autohide = document.querySelector('.autohide');
+    // add padding-top to body (if necessary)
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    document.body.style.paddingTop = navbar_height + 'px';
+  
+    if(el_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+    }
+}); 
+
+
 /* Home Page: To pass the value of the search box to recipes page */
 $("#btn-query").click(function() {
     let query = $("#home-query").val();

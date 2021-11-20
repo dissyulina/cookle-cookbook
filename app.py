588cce44ff2316ab83953650a5a4ac332108549f
Dissy Ulina <dissyulina@gmail.com>
2021-11-20 10:20:11 +0000

    Set debug to false

--- app.py
+++ app.py
@@ -1001,4 +1001,4 @@ def delete_review(review_id):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
